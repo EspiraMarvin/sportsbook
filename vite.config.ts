3ef92b27a438e96a20ba5e4ea8db2d9165718cb1
EspiraMarvin <espiramarvin@gmail.com>
2026-05-04 15:10:36 +0300

    test: add unit test coverage

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,13 @@ export default defineConfig({
 		environment: 'jsdom',
 		include: ['src/**/*.{test,spec}.{js,ts}'],
 		globals: true,
-		setupFiles: ['./src/test/setup.ts']
+		setupFiles: ['./src/test/setup.ts'],
+		coverage: {
+			provider: 'v8',
+			reporter: ['text', 'html', 'lcov'],
+			include: ['src/**/*.{js,ts,svelte}'],
+			exclude: ['src/**/*.{test,spec}.{js,ts}', 'src/test/**']
+		}
 	},
 	resolve: {
 		conditions: ['browser']
